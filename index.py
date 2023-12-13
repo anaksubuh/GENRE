42e176d7b4778a15f22e9a778e1bdc354b96d6e7
anaksubuh <johanesseptiawan2828@gmail.com>
2023-12-14 01:49:33 +0700

    Genre Update

--- index.py
+++ index.py
@@ -55,9 +55,7 @@ if selected == 'Dasboard':
 
     # Add custom CSS to hide the GitHub icon
     hide_github_icon = """
-    #GithubIcon {
-    visibility: hidden;
-    }
+    #GithubIcon {visibility: hidden;}
     """
     st.markdown(hide_github_icon, unsafe_allow_html=True)
 
