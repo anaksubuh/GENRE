57e43772d4f0ee6121e024b4d6aa2f4d008c777a
anaksubuh <johanesseptiawan2828@gmail.com>
2024-02-15 23:23:07 +0700

    first commit

--- push.bat
+++ push.bat
@@ -1,7 +1,6 @@
 git init
 git add .
-git commit -m "Genre Update"
+git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/anaksubuh/GENRE.git
 git push -u origin main
-git pull
---
+++ start.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('cls')
+os.system('python -m streamlit run streamlit_app.py')
+#os.system('python -m streamlit run telebot.py')
